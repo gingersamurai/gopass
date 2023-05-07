167a66e8e323c89be88b63193ba92780d54c11f2
gingersamurai <nazimmalyshev47@gmail.com>
2023-05-07 20:52:19 +0300

    remade entity.User

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -9,9 +9,9 @@ type Account struct {
 }
 
 type User struct {
-	Id       int64
-	Login    string
-	Password string
+	Id           int64
+	Login        string
+	PasswordHash string
 }
 
 type Service struct {
